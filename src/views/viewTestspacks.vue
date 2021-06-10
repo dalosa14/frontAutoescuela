f7b1abc461a5f872aa4a5c6d4650d09231aed7ba
saul <sagar5554@gmail.com>
2021-06-10 02:00:39 +0200

    titulos y estilos

--- src/views/viewTestspacks.vue
+++ src/views/viewTestspacks.vue
@@ -2,7 +2,7 @@
 
 navBar(class="lg:h-1/6 h-2/6")
 div(class="lg:h-5/6 h-4/6")
-  carrousel( :elements="elements" v-if="elements[0]" type="ver" title="MIS TESTS DISPONIBLES")
+  carrousel( :elements="elements" v-if="elements[0]" type="ver" title="MIS PAQUETES DE TESTS DISPONIBLES")
   h1(class="mx-auto p-6 h-full sm:p-10 lg:p-24 text-center  text-5xl text-indigo-600 border bg-gray-50" v-if="elements.length == 0") No tienes ningún paquete comprado
 
 </template>
