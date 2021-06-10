a4860fea806b7132ea7db4990641ee2629c01e29
saul <sagar5554@gmail.com>
2021-06-10 02:06:44 +0200

    borrar servicios y cambiar config

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,5 @@
 // Modules
 export * as userApi from "./services/user.service.js";
-export * as plansApi from "./services/plans.service.js";
-export * as webApi from "./services/web.service.js";
+
 export * as testPackage from "./services/testPackage.service.js";
-export * as profileApi from "./services/profile.service.js";
+
